bd01fbb1082e00eca8effd49e155746b7a9cd981
r9988.2588 <r9988.2588@gmail.com>
2024-12-25 11:44:18 +0000

    almost completed the index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,23 @@
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper"
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const RootLayout = () => {
   return (
     <PaperProvider>
-      <Stack>
-        <Stack.Screen name="index" />
-      </Stack>
+      <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar style="light" backgroundColor="black" />
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+
+          <Stack.Screen
+            name="Signin"
+            options={{ headerShown: false, headerTransparent: true }}
+          />
+          <Stack.Screen name="Signup" />
+        </Stack>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,96 @@
-import { Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { Text, View, StyleSheet, Image, } from "react-native";
+import { TouchableRipple } from "react-native-paper";
+import { router } from "expo-router";
 
 const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-        I am button
-      </Button>
+    // for background color
+    <View style={styles.background}>
+
+      {/* main container of the screen*/}
+      <View style={styles.container}>
+        {/* X white image */}
+        <View style={styles.imgCont}>
+          <Image
+            source={require("../assets/images/X-white.png")}
+            style={{ height: 35, width: 35, marginTop: 5 }}
+          />
+
+        </View>
+
+        {/* showing the text */}
+        <View style={styles.textView}>
+          <Text style={styles.text}>See what's happening</Text>
+          <Text style={styles.text}>in the world right now.</Text>
+        </View>
+
+        <View style={styles.authOptions}>
+
+          {/* Sign up btn */}
+          <TouchableRipple
+            style={styles.btn}
+            rippleColor="#00000040"
+            onPress={() => router.push("/auth/Signup")}>
+            <Text style={styles.btnText}>Sign up</Text>
+          </TouchableRipple>
+
+          {/* Sign in btn */}
+          <TouchableRipple
+            style={styles.btn}
+            rippleColor="#00000040"
+            onPress={() => router.push("/auth/Signin")}>
+            <Text style={styles.btnText}>Sign in</Text>
+          </TouchableRipple>
+        </View>
+      </View>
     </View>
   );
 }
 
 export default Index
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#000",
+    height: '100%'
+  },
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  imgCont: {},
+
+  textView: {
+    marginTop: 200,
+    // borderWidth: 1,
+    // borderColor: 'white'
+  },
+
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  authOptions: {
+    marginVertical: 150,
+  },
+
+  btn: {
+    backgroundColor: 'white',
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    width: '80%',
+    borderRadius: 50,
+  },
+
+  btnText: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
