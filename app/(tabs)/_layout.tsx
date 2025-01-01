7cdb2de2dbd160aa2459800d60f643128cb31fc3
r9988.2588 <r9988.2588@gmail.com>
2025-01-01 09:00:15 +0000

    added icons for tab bar screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,51 @@
 import { Tabs } from "expo-router";
+// icons
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 const TabLayout = () => {
     return (
-        <Tabs>
-            <Tabs.Screen name="Home" />
-            <Tabs.Screen name="Search" />
-            <Tabs.Screen name="Notifications" />
-            <Tabs.Screen name="Messages" />
+        <Tabs screenOptions={{
+            tabBarLabel: '',
+            tabBarStyle: {
+                backgroundColor: 'black',
+                borderWidth: 0,
+                paddingVertical: 10
+            },
+            tabBarActiveTintColor: 'white',
+            tabBarInactiveTintColor: 'white',
+        }}>
+            <Tabs.Screen name="Home"
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen name="Search"
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons name={focused ? 'search' : 'search-outline'} size={size} color={color} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen name="Notifications"
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons name={focused ? 'notifications' : 'notifications-outline'} size={size} color={color} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen name="Messages"
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <FontAwesome name={focused ? 'envelope' : 'envelope-o'} size={size} color={color} />
+                    ),
+                }}
+            />
         </Tabs>
     )
 }
