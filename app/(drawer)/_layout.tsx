a1742105a6b065e66f8368b7abd1977345231f02
r9988.2588 <r9988.2588@gmail.com>
2025-01-02 14:39:02 +0000

    added icons to drawer nav and profile

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,6 +1,8 @@
 import { Drawer } from "expo-router/drawer";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import CustomDrawerContent from "@/components/DrawerComponent/CustomDrawerContent";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import XWhite from "@/components/XWhite";
 
 const DrawerLayout = () => {
     return (
@@ -20,19 +22,38 @@ const DrawerLayout = () => {
                 {/* Tab Navigator */}
                 <Drawer.Screen
                     name="(tabs)"
-                    options={{ headerShown: false, title: "Home" }}
+                    options={{
+                        headerShown: false,
+                        title: "Home",
+                        drawerIcon: () => (
+                            <Ionicons name="home" size={24} color="white" />
+                        )
+                    }}
                 />
 
                 {/* Profile Screen */}
                 <Drawer.Screen
                     name="Profile"
-                    options={{ title: "Profile" }}
+                    options={{
+                        title: "Profile",
+                        drawerIcon: () => (
+                            <Ionicons name="person" size={24} color="white" />
+                        )
+                    }}
                 />
 
                 {/* Premium Screen */}
                 <Drawer.Screen
                     name="Premium"
-                    options={{ title: "Premium" }}
+                    options={{
+                        title: "Premium",
+                        drawerIcon: () => (
+                            <XWhite />
+                        ),
+                        drawerStyle: {
+                            marginRight: 10
+                        }
+                    }}
                 />
             </Drawer>
         </GestureHandlerRootView>
--- components/DrawerComponent/CustomDrawerContent.tsx
+++ components/DrawerComponent/CustomDrawerContent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
 
 const CustomDrawerContent = (props) => {
@@ -7,6 +7,11 @@ const CustomDrawerContent = (props) => {
         <DrawerContentScrollView {...props} contentContainerStyle={styles.container}>
             {/* Profile Section */}
             <View style={styles.profileSection}>
+                <Image
+                    source={{ uri: 'https://p6.focus.de/img/wissen/diverses/id_260334382/elon-musk-will-in-zwei-jahren-fuenf-unbemannte-missionen-zum-mars-schicken.-archivbild-.jpg?im=Resize%3D%28800%2C532%29&impolicy=perceptual&quality=mediumHigh&hash=db9a709ddeed554fab887833b4f6938665d1859233ee4401418d4114a2d0c6a4' }}
+
+                    style={{ width: 60, height: 60, borderRadius: 50, marginBottom: 10 }}
+                />
                 <Text style={styles.profileName}>Elon Musk</Text>
             </View>
 
@@ -28,7 +33,6 @@ const styles = StyleSheet.create({
     profileSection: {
         alignItems: "center",
         paddingVertical: 20,
-        backgroundColor: "#1a1a1a",
     },
     profileName: {
         color: "white",
